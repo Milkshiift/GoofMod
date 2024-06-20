713dd175fbc61b7e5b36da049b72a48da752e262
Milkshift <99212308+Milkshiift@users.noreply.github.com>
2024-06-20 17:38:31 +0300

    Fix 15_richPresence

--- patches/15_richPresence.js
+++ patches/15_richPresence.js
@@ -1,4 +1,4 @@
-const RpcUtils = Vencord.Webpack.findByPropsLazy("fetchApplicationsRPC", "getRemoteIconURL");
+const fetchApplicationsRPC = Vencord.Webpack.findByCodeLazy("APPLICATION_RPC(", "Client ID");
 
 async function lookupAsset(applicationId, key) {
     return (await Vencord.Webpack.Common.ApplicationAssetUtils.fetchAssetIds(applicationId, [key]))[0];
@@ -7,29 +7,26 @@ async function lookupAsset(applicationId, key) {
 const apps = {};
 async function lookupApp(applicationId) {
     const socket = {};
-    await RpcUtils.fetchApplicationsRPC(socket, applicationId);
+    await fetchApplicationsRPC(socket, applicationId);
     return socket.application;
 }
 
 goofcord.rpcListen(async (data) => {
-    msg = JSON.parse(data);
+    data = JSON.parse(data);
 
-    if (msg.activity?.assets?.large_image)
-        msg.activity.assets.large_image = await lookupAsset(
-            msg.activity.application_id,
-            msg.activity.assets.large_image
-        );
-    if (msg.activity?.assets?.small_image)
-        msg.activity.assets.small_image = await lookupAsset(
-            msg.activity.application_id,
-            msg.activity.assets.small_image
-        );
-    if (msg.activity) {
-        const appId = msg.activity.application_id;
-        if (!apps[appId]) apps[appId] = await lookupApp(appId);
+    const { activity } = data;
+    const assets = activity?.assets;
+
+    if (assets?.large_image) assets.large_image = await lookupAsset(activity.application_id, assets.large_image);
+    if (assets?.small_image) assets.small_image = await lookupAsset(activity.application_id, assets.small_image);
+
+    if (activity) {
+        const appId = activity.application_id;
+        apps[appId] ||= await lookupApp(appId);
+
         const app = apps[appId];
-        if (!msg.activity.name) msg.activity.name = app.name;
+        activity.name ||= app.name;
     }
 
-    Vencord.Webpack.Common.FluxDispatcher.dispatch({type: "LOCAL_ACTIVITY_UPDATE", ...msg});
+    Vencord.Webpack.Common.FluxDispatcher.dispatch({type: "LOCAL_ACTIVITY_UPDATE", ...data});
 });
