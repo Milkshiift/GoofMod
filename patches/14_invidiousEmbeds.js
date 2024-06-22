ad455af5295975cba935bf2ab422fd52c6acf6d1
Milkshift <99212308+Milkshiift@users.noreply.github.com>
2024-06-22 14:02:00 +0300

    Update 14_invidiousEmbeds.js

--- patches/14_invidiousEmbeds.js
+++ patches/14_invidiousEmbeds.js
@@ -1,7 +1,6 @@
 /**
  * @name InvidiousEmbeds
- * @description Replaces youtube embeds with invidious embeds
- * @version 1.0.2
+ * @description Replaces YouTube embeds with Invidious embeds
  */
 
 const patches = [
@@ -10,9 +9,44 @@ const patches = [
         replacement: [
             {
                 match: /(:.,src:.\.url)/,
-                replace: "$1.replace('https://www.youtube.com', 'https://farside.link/invidious')+'?player_style=youtube'"
+                replace: "$1.replace('https://www.youtube.com/embed/', window.InvidiousEmbeds.fastestInstance())+'&local=true'"
             }
         ],
         plugin: "InvidiousEmbeds"
     }
 ];
+
+// TODO: Make fastestInstanceCached persistent across restarts
+
+let fastestInstanceCached = "https://farside.link/invidious/latest_version?id=";
+async function updateFastestInstance() {
+    const response = await fetch("https://api.invidious.io/instances.json?pretty=0&sort_by=type,users");
+    const json = await response.json();
+    const instances = json.map((instance) => instance[1].uri);
+
+    console.log("Testing Invidious instances...");
+    let prevBestTime = Infinity;
+    let prevBestInstance = null;
+    for (const instance of instances) {
+        if (!instance.includes("https")) continue;
+        const start = performance.now();
+        try {
+            await fetch(instance+"/embed/ekQHJX9rMp8");
+        } catch (e) {}
+        const end = performance.now();
+        const time = end - start;
+        //console.log(instance, time);
+        if (time < prevBestTime) {
+            prevBestTime = time;
+            prevBestInstance = instance;
+        }
+    }
+
+    console.log("Fastest instance:", prevBestInstance, prevBestTime);
+    fastestInstanceCached = prevBestInstance+"/latest_version?id=";
+}
+
+updateFastestInstance();
+
+window.InvidiousEmbeds = {};
+window.InvidiousEmbeds.fastestInstance = () => fastestInstanceCached;
